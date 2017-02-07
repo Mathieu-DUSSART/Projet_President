ec8b0a1c1e8130d67531c21baee1b4933721ee7a
arsène <arsenegrandon@gmail.com>
2017-02-07 16:29:26 +0100

    refonte de le class index

---
+++ include/header.inc.php
@@ -0,0 +1,24 @@
+<?php session_start();
+ ?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+
+  <meta charset="utf-8">
+
+<?php
+		$title = "";?>
+		<title>
+		<?php echo $title ?>
+		</title>
+
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
+</head>
+	<body>
+        <header>
+
+        </header>
+
+        <div id="corps">
--- index.php
+++ index.php
@@ -1,35 +1,13 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <?php
-    session_start();
+
+<?php
+
     require_once("include/config.inc.php");
     require_once("include/autoLoad.inc.php");
     require_once("include/function.inc.php");
     $pdo=new Mypdo();
-    ?>
-    <head>
-        <title>Président</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        
-        <!-- Import JQuery-->
-        <script src="/Projet_President/js/jquery-3.1.1.min.js"></script>
+    require_once("include/header.inc.php");
 
-        <!-- Import JQueryUI-->
-        <script src="/Projet_President/js/JQueryUI/jquery-ui.min.js"></script>
-        <link rel="stylesheet" href="/Projet_President/js/JQueryUI/jquery-ui.min.css">
 
-        <script src="js/function.js"></script>
-    </head>
-    <body>
-        <header>
-            
-        </header>
+    require_once("include/footer.inc.php");
 
-        <div id="corps">
-      
-
-           
-        </div>
-    </body>
-</html>
+?>
