74578502798dee1ee0e4d3fa943d326d57518bc2
babadu87 <baptiste.boismorand@etu.unilim.fr>
2017-02-08 16:47:19 +0100

    manager connexion

--- classes/ConnexionManager.class.php
+++ classes/ConnexionManager.class.php
@@ -8,15 +8,32 @@ class ArticleManager{
 	}
 	
 public function checkLoginPassword($login, $password){
-        $password = crypterPassword($password);
-        $sql = "SELECT joueur_id FROM joueur WHERE joueur_pseudo = :login AND joueur_password = :password";
-        $req = $this->db->prepare($sql);
-        $req->bindValue(':login', $login, PDO::PARAM_STR);
-        $req->bindValue(':password', $password, PDO::PARAM_STR);
-        $req->execute();
-        $resu = $req->fetch(PDO::FETCH_OBJ);
+	$sql = "SELECT joueur_id FROM joueur WHERE joueur_pseudo = :login AND joueur_password = :password";
+	$req = $this->db->prepare($sql);
+	$req->bindValue(':login', $login, PDO::PARAM_STR);
+	$req->bindValue(':password', $password, PDO::PARAM_STR);
+	$req->execute();
+	$resu = $req->fetch(PDO::FETCH_OBJ);
 
-        return (isset($resu->joueur_id));
-    }
+	return (isset($resu->joueur_id));
+}
+	
+public function addLoginPassword($login, $password){
+	$sql="INSERT INTO Joueur(joueur_pseudo, joueur_password,) VALUES(:login, :password, )";
+	$req=$this->db->prepare($sql);
+	$req->bindValue(':login', $login, PDO::PARAM_STR);
+	$req->bindValue(':password', $password, PDO::PARAM_STR);
+	$req->execute();
+}
+
+public function existeLogin($login){
+	$sql = "SELECT joueur_id FROM joueur WHERE joueur_pseudo = :login";
+	$req = $this->db->prepare($sql);
+	$req->bindValue(':login', $login, PDO::PARAM_STR);
+	$req->bindValue(':password', $password, PDO::PARAM_STR);
+	$req->execute();
+	$resu = $req->fetch(PDO::FETCH_OBJ);
+
+	return (isset($resu->joueur_id));
 }
 ?>
