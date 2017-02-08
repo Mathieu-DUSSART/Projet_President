5c0fa907c21ee5242a02c973f5bd730959f6bb03
babadu87 <baptiste.boismorand@etu.unilim.fr>
2017-02-08 17:22:06 +0100

    manager

--- classes/MainManager.class.php
+++ classes/MainManager.class.php
@@ -10,16 +10,16 @@ class CarteManager{
 	public function addCarte($idJoueur, $idCarte){
 		$sql="INSERT INTO main(joueur_id, carte_id) VALUES(:idJoueur, :idCarte)";
         $req=$this->db->prepare($sql);
-        $req->bindValue(':idJoueur', $article->getTitre(), PDO::PARAM_INT);
-        $req->bindValue(':idCarte', $article->getDate(), PDO::PARAM_INT);
+        $req->bindValue(':idJoueur', joueur_id, PDO::PARAM_INT);
+        $req->bindValue(':idCarte', carte_id, PDO::PARAM_INT);
         $req->execute();
 	}
 	
 	public function addCarte($idJoueur, $idCarte){
 		$sql="DELETE FROM main where joueur_id=:idJoueur, carte_id=:idCarte)";
         $req=$this->db->prepare($sql);
-        $req->bindValue(':idJoueur', $article->getTitre(), PDO::PARAM_INT);
-        $req->bindValue(':idCarte', $article->getDate(), PDO::PARAM_INT);
+        $req->bindValue(':idJoueur', joueur_id, PDO::PARAM_INT);
+        $req->bindValue(':idCarte', carte_id, PDO::PARAM_INT);
         $req->execute();
 	}
 }
