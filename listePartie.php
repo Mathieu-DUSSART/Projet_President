6d885be711be286593acfa765410945e776c1d64
babadu87 <baptiste.boismorand@etu.unilim.fr>
2017-02-10 15:21:28 +0100

    script php listerPartie

---
+++ listePartie.php
@@ -0,0 +1,11 @@
+<?php
+require_once("include/config.inc.php");
+require_once("include/autoLoad.inc.php");
+$pdo = new Mypdo();
+$managerPartie = new PartieManager($pdo);
+
+$tabPartie = $managerPartie->getPartie();
+
+
+
+}
