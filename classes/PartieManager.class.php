ee30007fcf2fca38c1ba7dedd954838d6b3977b1
Mathieu-DUSSART <mathieud17700@gmail.com>
2017-02-10 14:20:50 +0100

    Fuck

--- classes/PartieManager.class.php
+++ classes/PartieManager.class.php
@@ -16,13 +16,13 @@ class PartieManager{
 		}
 		return $tabPartie;
 	}
-	
+
 	public function getNbJoueurPartie(partie_id){
 		$sql="SELECT count(joueur_id) as nbJoueur FROM joueurpartie where partie_id =:partie_id ";
 		$req=$this->db->prepare($sql);
         $req->bindValue(':partie_id', partie_id, PDO::PARAM_INT);
         $req->execute();
-		$res=$req->fetch(PDO::FETCH_OBJ)
+		$res=$req->fetch(PDO::FETCH_OBJ);
 		return $res;
 	}
 }
