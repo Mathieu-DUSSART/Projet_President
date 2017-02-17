3bcbfd33a2b5e499730d85e4d942cfc8162dd4b7
Mathieu-DUSSART <mathieud17700@gmail.com>
2017-02-17 13:58:26 +0100

    index

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
                                 $("#resultat").html("<p>Vous avez été connecté avec succès !</p>");
                                 $(".divConnexion").hide();
                                 $(".divPartie").toggle();
-                                
+
                             }else{
                                 // Le membre n'a pas été connecté. (data vaut ici "failed")
                                 $("#resultat").html("<p>Erreur lors de la connexion...</p>");
