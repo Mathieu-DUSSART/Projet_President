88f160fd58d89a8bbb5bf3ba02f642e3f94a483d
arsène <arsenegrandon@gmail.com>
2017-03-09 10:22:38 +0100

    ajout fichier connexion

---
+++ include/pages/connexion.inc.php
@@ -0,0 +1,15 @@
+<h1>Se connecter</h1>
+
+<form id="formConnexion" method="POST" action="#" autocomplete="off">
+    <label>Login :</label>
+    <input class = "champ" type="text" name="login" required>
+    <br>
+    <label>Mot de passe :</label>
+    <input class="champ" type="password" name="password" required>
+    <br>
+    <input class="bouton" type='button' id="submit" value='Connexion'>
+</form>
+
+<div id="resultat">
+    <!-- Nous allons afficher un retour en jQuery au visiteur -->
+</div>
